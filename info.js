7833724809e109d87a82f17231288c2de2b23b35
mohith1490 <mohith1754singh@gmail.com>
2024-07-22 14:18:11 +0530

    new cool stuf

---
+++ info.js
@@ -0,0 +1,7 @@
+ const data = {
+    user1:'example@gmail.com',
+    user2:'example@gmail.com',
+    user3:'example@gmail.com'
+}
+
+export default data
---
+++ server.js
@@ -0,0 +1,41 @@
+import nodemailer from 'nodemailer'
+import http from 'http'
+import data from './info.js'
+
+const server = http.createServer((request, response) => {
+    const auth = nodemailer.createTransport({
+        service: "gmail",
+        secure : true,
+        port : 587,
+        auth: {
+            user: "mohith1754singh@gmail.com",
+            pass: "pass key of gmail"
+
+        }
+    });
+let receiver
+
+
+for (const key in data) {
+   receiver = {
+        from : "mohith1754singh@gmail.com",
+        to : `${data[key]}`,
+        subject : "testing the service",
+        text : "Hello this is mohith!"
+    };
+}
+
+    
+
+    auth.sendMail(receiver, (error, emailResponse) => {
+        if(error){
+        throw error;
+    }
+        console.log("success!");
+        response.end();
+    });
+    
+});
+
+server.listen(8080);
+
